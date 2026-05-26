c088553c46a0bada1872792530e9bf9796fadb73
Nane <naneth.martelo@udea.edu.co>
2026-05-26 17:35:17 -0500

    Revert "Implement sprint 3 shipment reports"
    This reverts commit 12f5d22a688b468a6eae47e268ef4e4ba9bda7bb.

--- tackingLogisticoBackEnd-main/trackingLogisticoEap11/src/main/java/com/logistica/trackinglogistico/users/repository/PersonRepository.java
+++
@@ -1,8 +0,0 @@
-package com.logistica.trackinglogistico.users.repository;
-
-import com.logistica.trackinglogistico.users.model.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person, Integer> {
-    boolean existsByTelefono(String telefono);
-}
