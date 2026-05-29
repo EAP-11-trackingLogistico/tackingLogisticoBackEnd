8d05d18200d6134bce848c3e27c0060138dd9f78
Diego Pulgarín <diego.pulgarinj@udea.edu.co>
2026-05-28 20:31:29 -0500

    refactor: reducir duplicación de código y mejorar mantenibilidad

--- trackingLogisticoEap11/src/main/java/com/logistica/trackinglogistico/users/dto/CreatePersonRequest.java
+++ trackingLogisticoEap11/src/main/java/com/logistica/trackinglogistico/users/dto/CreatePersonRequest.java
@@ -1,7 +1,11 @@
 package com.logistica.trackinglogistico.users.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 public class CreatePersonRequest {
 
     @NotBlank
@@ -13,27 +17,4 @@ public class CreatePersonRequest {
     @NotBlank
     private String telefono;
 
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
 }
--- trackingLogisticoEap11/src/main/java/com/logistica/trackinglogistico/users/model/Person.java
+++ trackingLogisticoEap11/src/main/java/com/logistica/trackinglogistico/users/model/Person.java
@@ -1,7 +1,11 @@
 package com.logistica.trackinglogistico.users.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Entity
 @Table(name = "persona")
 public class Person {
@@ -24,35 +28,4 @@ public class Person {
         // Constructor vacío requerido por JPA
     }
 
-    public Integer getIdPersona() {
-        return idPersona;
-    }
-
-    public void setIdPersona(Integer idPersona) {
-        this.idPersona = idPersona;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
 }
